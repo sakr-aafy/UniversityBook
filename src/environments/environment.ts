5da49c44a55b99ad7803d4de7da7f8c334e50d97
sakr-aafy <sakr.aafy@gmail.com>
2026-08-21 12:38:46 +0100

    Point apiUrl at the new backend instance

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://caisse-backend-mn10.onrender.com/api',
+  apiUrl: 'https://caisse-backend-1.onrender.com/api',
+  //apiUrl: 'https://caisse-backend-mn10.onrender.com/api',
   fileBaseUrl: 'http://localhost:5000'
 };
